de794caf015ef3079bf3d1feab5c7122db760139
martji <magq1001@gmail.com>
2016-03-29 23:29:27 +0800

    ADD: 1. 初步实现改良版 SWFP 算法

---
+++ rmemcached_server/src/main/java/com/sdp/hotspot/SWFPDetectorImp.java
@@ -0,0 +1,104 @@
+package com.sdp.hotspot;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Guoqing on 2016/3/29.
+ */
+public class SWFPDetectorImp implements BaseFrequentDetector {
+
+    private ConcurrentHashMap<String, SWFPCounter> swfpMap = new ConcurrentHashMap<String, SWFPCounter>();
+
+    private int counterSize = 10000;
+
+    private int threshold = 1000;
+
+    private int sliceTime = 5*1000;
+
+    public void initConfig() {
+
+    }
+
+    public boolean registerItem(String key) {
+        if (swfpMap.containsKey(key)) {
+            swfpMap.get(key).add();
+            if (swfpMap.get(key).getCount() > threshold) {
+                itemCounters.put(key, swfpMap.get(key).frequent);
+            }
+        } else {
+            if (swfpMap.size() < counterSize) {
+                swfpMap.put(key, new SWFPCounter(key));
+            } else {
+                Set<String> keySet = swfpMap.keySet();
+                for (String item: keySet) {
+                    swfpMap.get(item).del();
+
+                    if (swfpMap.get(item).frequent <= 0) {
+                        swfpMap.remove(item);
+                    }
+                }
+
+                if (swfpMap.size() < counterSize) {
+                    swfpMap.put(key, new SWFPCounter(key));
+                }
+            }
+        }
+        if (itemCounters.containsKey(key)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void refreshSWFPCounter() {
+        Set<String> keySet = swfpMap.keySet();
+        for (String item: keySet) {
+            swfpMap.get(item).refresh();
+        }
+    }
+
+    public ConcurrentHashMap<String, Integer> getItemCounters() {
+        return itemCounters;
+    }
+
+    public void refreshItemCounters() {
+        itemCounters.clear();
+    }
+
+    static class SWFPCounter implements Comparable<SWFPCounter>{
+        private String key;
+        private int frequent;
+        private int dfrequent;
+        private int prefrequent;
+
+        public SWFPCounter(String key) {
+            this.key = key;
+            this.frequent = 1;
+            this.dfrequent = 0;
+            this.prefrequent = 0;
+        }
+
+        public void add () {
+            frequent += 1;
+        }
+
+        public void del() {
+            frequent -= 1;
+            dfrequent += 1;
+        }
+
+        public void refresh() {
+            dfrequent = 0;
+            prefrequent = prefrequent/2 + frequent/2;
+            frequent = frequent/2;
+        }
+
+        public int getCount() {
+            return frequent + dfrequent + prefrequent;
+        }
+
+        public int compareTo(SWFPCounter o) {
+            return o.getCount() - getCount();
+        }
+    }
+}
