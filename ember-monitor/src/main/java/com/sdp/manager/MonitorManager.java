01be613d58e5d5e22b34893240f2825ae104e6e4
martji <magq1001@gmail.com>
2016-12-05 17:10:26 +0800

    Fix monitor

---
+++ ember-monitor/src/main/java/com/sdp/manager/MonitorManager.java
@@ -0,0 +1,117 @@
+package com.sdp.manager;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sdp.common.EMSGID;
+import com.sdp.messagebody.CtsMsg.nr_cpuStats;
+import com.sdp.netty.NetMsg;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.util.internal.ConcurrentHashMap;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Map.Entry;
+
+/**
+ * @author martji
+ */
+public class MonitorManager extends Thread {
+
+    ConcurrentHashMap<Integer, Channel> serverChannelMap = new ConcurrentHashMap<Integer, Channel>();
+    Map<Integer, Queue<Double>> cpuCostMap = new HashMap<Integer, Queue<Double>>();
+    Map<Integer, Double> medianCpuCostMap = new HashMap<Integer, Double>();
+
+    public MonitorManager() {
+
+    }
+
+    public void run() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        while (true) {
+            try {
+                System.out.println(df.format(new Date()) + ": [CPUCost] " + medianCpuCostMap.toString());
+                Thread.sleep(1000 * 5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Iterator<Entry<Integer, Channel>> servers = serverChannelMap.entrySet().iterator();
+            while (servers.hasNext()) {
+                Entry<Integer, Channel> server = servers.next();
+                Channel channel = server.getValue();
+                if (channel.isConnected()) {
+                    nr_cpuStats.Builder builder = nr_cpuStats.newBuilder();
+                    NetMsg send = NetMsg.newMessage();
+                    send.setMessageLite(builder);
+                    send.setMsgID(EMSGID.nr_stats);
+                    channel.write(send);
+                } else {
+                    int serverId = server.getKey();
+                    System.out.println(df.format(new Date()) + ": [Netty] Server " + serverId + " lose connect.");
+                    serverChannelMap.remove(serverId);
+                    medianCpuCostMap.remove(serverId);
+                    cpuCostMap.remove(serverId);
+                }
+            }
+        }
+    }
+
+    public void addServer(int clientNode, Channel channel) {
+        serverChannelMap.put(clientNode, channel);
+    }
+
+    /**
+     * @param clientNode
+     * @param cpuCost
+     */
+    public void handle(int clientNode, String cpuCost) {
+        Double cost = Double.parseDouble(cpuCost);
+        if (cpuCostMap.containsKey(clientNode)) {
+            Queue<Double> arrCpuCost = cpuCostMap.get(clientNode);
+            arrCpuCost.offer(cost);
+            if (arrCpuCost.size() > 10) {
+                arrCpuCost.poll();
+            }
+            medianCpuCostMap.put(clientNode, getData((medianCpuCostMap.get(clientNode) + cost) / 2));
+        } else {
+            Queue<Double> arryCpuCost = new LinkedList<Double>();
+            arryCpuCost.offer(cost);
+            cpuCostMap.put(clientNode, arryCpuCost);
+            medianCpuCostMap.put(clientNode, getData(cost));
+        }
+    }
+
+    /**
+     * @param clientNode
+     */
+    public int chooseReplica(int clientNode) {
+        int replicaNum = -1;
+        Iterator<Entry<Integer, Double>> medianCosts = medianCpuCostMap.entrySet().iterator();
+        while (medianCosts.hasNext()) {
+            Entry<Integer, Double> costMap = medianCosts.next();
+            if (costMap.getKey() != clientNode) {
+                if (replicaNum == -1) {
+                    replicaNum = costMap.getKey();
+                } else if (costMap.getValue() < medianCpuCostMap.get(replicaNum)) {
+                    replicaNum = costMap.getKey();
+                }
+            }
+        }
+        return replicaNum;
+    }
+
+    public String chooseReplica() {
+        if (medianCpuCostMap.isEmpty()) {
+            return "";
+        }
+        Gson gson = new GsonBuilder().enableComplexMapKeySerialization().create();
+        String result = gson.toJson(medianCpuCostMap);
+        return result;
+    }
+
+    public double getData(double data) {
+        BigDecimal b = new BigDecimal(data);
+        return b.setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+}
---
+++ ember-monitor/src/main/java/com/sdp/server/MonitorServerHandler.java
@@ -0,0 +1,87 @@
+package com.sdp.server;
+
+import com.sdp.common.EMSGID;
+import com.sdp.messagebody.CtsMsg.nr_apply_replica;
+import com.sdp.messagebody.CtsMsg.nr_apply_replica_res;
+import com.sdp.messagebody.CtsMsg.nr_cpuStats_res;
+import com.sdp.messagebody.StsMsg.nm_connected_mem_back;
+import com.sdp.manager.MonitorManager;
+import com.sdp.netty.NetMsg;
+import org.apache.log4j.Logger;
+import org.jboss.netty.channel.*;
+
+/**
+ * @author martji
+ */
+
+public class MonitorServerHandler extends SimpleChannelUpstreamHandler {
+
+    Logger logger;
+    MonitorManager monitor;
+
+    public MonitorServerHandler() {
+        logger = Logger.getLogger(MonitorServerHandler.class);
+
+        monitor = new MonitorManager();
+        monitor.start();
+    }
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+        handleMessage(e);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+        Channel channel = e.getChannel();
+        channel.close();
+    }
+
+    @Override
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e)
+            throws Exception {
+    }
+
+    private void handleMessage(MessageEvent e) {
+        NetMsg msg = (NetMsg) e.getMessage();
+        switch (msg.getMsgID()) {
+            case nm_connected: {
+                int clientNode = msg.getNodeRoute();
+                System.out.println("Register info: instanceId: " + clientNode + " channel: " + e.getChannel());
+                monitor.addServer(clientNode, e.getChannel());
+
+                nm_connected_mem_back.Builder builder = nm_connected_mem_back.newBuilder();
+                NetMsg send = NetMsg.newMessage();
+                send.setMessageLite(builder);
+                send.setMsgID(EMSGID.nm_connected_mem_back);
+
+                e.getChannel().write(send);
+            }
+            break;
+            case nr_stats_res: {
+                int clientNode = msg.getNodeRoute();
+                nr_cpuStats_res msgLite = msg.getMessageLite();
+                String cpuCost = msgLite.getValue();
+                monitor.handle(clientNode, cpuCost);
+            }
+            break;
+            case nr_apply_replica: {
+                String replicas = monitor.chooseReplica();
+                nr_apply_replica msgLite = msg.getMessageLite();
+                String id = msgLite.getKey();
+
+                nr_apply_replica_res.Builder builder = nr_apply_replica_res.newBuilder();
+                builder.setKey(id);
+                builder.setValue(replicas);
+                NetMsg send = NetMsg.newMessage();
+                send.setMessageLite(builder);
+                send.setMsgID(EMSGID.nr_apply_replica_res);
+                e.getChannel().write(send);
+            }
+            break;
+            default:
+                break;
+        }
+    }
+}
+
